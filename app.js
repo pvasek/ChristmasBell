6848ac657cdc6512246e7455eacbf0e0074efdf8
pvasek <pvasek@gmail.com>
2013-12-24 01:54:49 +0100

    logging removed

--- app.js
+++ app.js
@@ -119,4 +119,4 @@ io.sockets.on('connection', function (socket) {
 
 });
 
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
